ad8dcade7ec211ed9be109b1d3a285d48f004688
JHDsBR <Jesse2013.jsc@gmail.com>
2022-09-17 14:18:29 -0300

    teste github env from file

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import os
 import shareData
 import sys
+from dotenv import load_dotenv
+from pathlib import Path
+
+path = os.environ["GITHUB_ENV"]
+dotenv_path = Path(path)
+load_dotenv(dotenv_path=dotenv_path)
 # os.environ["TESTE"] = "DEBUG" # não funcionou
 # shareData.CreateData("TESTE", "DEBUG") # funcionou 😎
 
