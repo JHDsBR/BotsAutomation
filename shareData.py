87b18fc5fc844b25680484a6a5e0c2f5f7d68000
JHDsBR <Jesse2013.jsc@gmail.com>
2022-09-17 12:12:47 -0300

    json para compatilhar variáveis entre scripts

---
+++ shareData.py
@@ -0,0 +1,14 @@
+import json
+
+
+def AddNewData(var, value):
+    
+    with open("envData.json") as file:
+        oldData = json.load(file)
+    
+    oldData[var] = value
+    oldData = json.dumps(oldData, indent=4)
+
+    with open("envData.json", "w") as file:
+        file.write(oldData)
+
