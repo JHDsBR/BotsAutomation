31d7e5812eefd21bbcdc0b07e88fd9035850be4b
JHDsBR <Jesse2013.jsc@gmail.com>
2022-09-18 13:19:31 -0300

    correção :D

--- main.py
+++ main.py
@@ -23,17 +23,17 @@ for c in range(20):
 # download do vídeo
 title = Download.Video()
 
-# pega um bot
-bot = Utils.GetBotToUploadVideo(randomly=True)
-if not bot:
-    Notify.SendEmail("BOT NOT FOUND", "Não foi possível encontrar um bot para fazer o upload do vídeo.")
-    exit()
-
-requests.post(MY_API_URL_UPDATE, json={"id":int(bot['id']), "newData":{"uploadVideo":True}})
-
 # upload do vídeo
 uploaded = False
 while not uploaded:
+
+    # pega um bot
+    bot = Utils.GetBotToUploadVideo(randomly=True)
+    if not bot:
+        Notify.SendEmail("BOT NOT FOUND", "Não foi possível encontrar um bot para fazer o upload do vídeo.")
+        exit()
+
+    requests.post(MY_API_URL_UPDATE, json={"id":int(bot['id'])})
     # fica tentando postar o vídeo até conseguir
     uploaded = StartUploadVideo(bot=bot,title=title)
 
