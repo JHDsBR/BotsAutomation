3d459f33ed8e7a612a27b3d628dd19bddecca8b6
JHDsBR <Jesse2013.jsc@gmail.com>
2022-09-19 23:51:44 -0300

    bot session
    a princípio seria implementando com js

---
+++ Session.py
@@ -0,0 +1,94 @@
+import requests as rq
+import json
+import datetime
+from constants import API_URL
+from os import environ as env
+
+#! REVISADO
+#! REVISADO
+#! REVISADO
+
+class BotSession():
+
+    def __init__(self, id) -> None:
+        self.id = id
+        self.session = rq.Session()
+        self.Login()
+
+
+    def Login(self):
+        
+        res = self.session.get(f"{env['MY_API_URL']}/{self.id}")
+        
+        if(res.status_code != 200):
+            return False
+
+        for cok in json.loads(res.json()["cookie"]):
+            self.session.cookies.set(cok["name"], cok["value"], domain=env["DOMINIO"])
+        
+        # é bom implementar uma verificação de login bem sucedido
+        
+
+    # necessário fazer login antes
+    def GetTokenCSRF(self):
+        return self.session.get(env["URL_API_TOKEN"]).json()["data"]["token"]
+
+
+    def GetAuth(self, body):
+        return rq.get(env["URL_API_AUTH"], json=body).json()
+
+
+    def Like(self, post_id:str):
+        id = post_id
+
+        t = int(datetime.datetime.now().timestamp())
+
+        body = {
+            "post_id":id,
+            "timestamp":t
+        }
+
+        headers = self.Headers(body)
+        # return
+ 
+        
+        res = self.session.post(API_URL.LIKE, json=body, headers=headers)
+        
+        # print(res.text)
+        ok = "success" in res.text.lower()
+        if not ok:
+            print(res.text)
+        return ok
+    
+
+
+    def Comment(self, post_id, comment):
+        id = post_id
+
+        t = int(datetime.datetime.now().timestamp())
+
+        body = {
+            "post_id":id,
+            "comment_content":comment,
+            "timestamp":t
+        }
+
+        headers = self.Headers(body)
+        # return
+        res = self.session.post(API_URL.COMMENT, json=body, headers=headers)
+        
+        # print(res.text)
+
+        if("success" in res.text.lower()):
+            return res.json()["data"]["replyid"]
+
+    def Headers(self, body):
+
+        return {
+            "X-Xsrf-Token":self.GetTokenCSRF(), 
+            "Authorization":self.GetAuth(body)
+        }
+
+    def Close(self):
+        self.session.close()
+
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import loadEnv
 
-from constants import MY_API_URL_UPDATE
+from random import sample
+from constants import *
 from time import sleep
 from UploadVideo import StartUploadVideo
+from Session import BotSession
 import Download
 import Utils
 import Notify
-import requests
+import requests as rq
 
 #! REVISADO
 #! REVISADO
@@ -33,8 +35,76 @@ while not uploaded:
         Notify.SendEmail("BOT NOT FOUND", "Não foi possível encontrar um bot para fazer o upload do vídeo.")
         exit()
 
-    requests.post(MY_API_URL_UPDATE, json={"id":int(bot['id'])})
+    rq.post(MY_API_URL_UPDATE, json={"id":int(bot['id'])})
     # fica tentando postar o vídeo até conseguir
     uploaded = StartUploadVideo(bot=bot,title=title)
 
 Notify.SendEmail("VIDEO UPLOADED", uploaded[1])
+
+
+
+video_id = -1
+comments_id = {}
+
+def Start(video_id_):
+    global video_id, comments_id
+    video_id = str(video_id_)
+    bots = rq.get(env["MY_API_URL"]).json()
+
+    for b in bots:
+
+        if(b["uploadVideo"] or b["cookieExpired"]):
+            continue
+        
+        bot_session = BotSession(b["id"])
+        if(bot_session.Like(video_id)):
+            print("Curti")
+        else:
+            print("Não curti")
+        sleep(TIME_BETWEEN_THE_LIKE_AND_COMMENT)
+        comment_id = bot_session.Comment(video_id, COMMENT.GetRandom())
+        
+        if(comment_id):
+            print("Comentei")
+            comments_id[b["id"]] = comment_id
+        else:
+            print("Não comentei")
+
+        bot_session.Close()
+
+        sleep(TIME_BETWEEN_LIKE_AND_COMMENT_VIDEO)
+    
+    print("Start like comments")
+    for b in bots:
+
+        random_ids = GetRandomIds(exclude_id=b["id"])
+
+        if(b["cookieExpired"]):
+            continue
+        
+        bot_session = BotSession(b["id"])
+
+        for _, post_id in random_ids:
+            if(bot_session.Like(post_id)):
+                print("Curti um comentário")
+                sleep(TIME_BETWEEN_LIKES_ON_COMMENTS)
+            else:
+                print("Não curti um comentário")
+
+        bot_session.Close()
+
+    video_id = -1
+
+
+def GetRandomIds(exclude_id):
+    global video_id, comments_id
+    
+    comments_id_copy = comments_id.copy()
+    
+    try:
+        del comments_id_copy[exclude_id]
+    except:
+        pass
+
+    return sample(comments_id_copy.items(), MAX_LIKES_ON_COMMENTS if len(list(comments_id_copy.items())) > MAX_LIKES_ON_COMMENTS else len(list(comments_id_copy.items())))
+    
