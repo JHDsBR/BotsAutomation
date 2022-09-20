3d4b2337df2cc64294f2c2e1e08619f135725a4d
JHDsBR <Jesse2013.jsc@gmail.com>
2022-09-20 00:11:15 -0300

    teste

--- Utils.py
+++ Utils.py
@@ -1,12 +1,18 @@
 import random
 from time import sleep
 import requests
+from Session import BotSession
+from constants import *
+from random import sample
+from time import sleep
+import requests as rq
 
-from constants import MY_API_URL
+#! REVISADO
+#! REVISADO
+#! REVISADO
 
-#! REVISADO
-#! REVISADO
-#! REVISADO
+video_id = -1
+comments_id = {}
 
 def Wait(minime=0, maxime=None):
     maxime = maxime if maxime else minime+random.random()*5
@@ -44,3 +50,66 @@ def GetBotToUploadVideo(randomly:bool=False):
             if not bot["cookieExpired"]:
                 return bot
             bots.remove(bot)
+
+def Start(video_id_):
+    global video_id, comments_id
+    video_id = str(video_id_)
+    bots = rq.get(env["MY_API_URL"]).json()
+
+    for b in bots:
+
+        if(b["uploadVideo"] or b["cookieExpired"]):
+            continue
+        
+        bot_session = BotSession(b["id"])
+        if(bot_session.Like(video_id)):
+            print("Curti")
+        else:
+            print("Não curti")
+        sleep(TIME_BETWEEN_THE_LIKE_AND_COMMENT)
+        comment_id = bot_session.Comment(video_id, COMMENT.GetRandom())
+        
+        if(comment_id):
+            print("Comentei")
+            comments_id[b["id"]] = comment_id
+        else:
+            print("Não comentei")
+
+        bot_session.Close()
+
+        sleep(TIME_BETWEEN_LIKE_AND_COMMENT_VIDEO)
+    
+    print("Start like comments")
+    for b in bots:
+
+        random_ids = GetRandomIds(exclude_id=b["id"])
+
+        if(b["cookieExpired"]):
+            continue
+        
+        bot_session = BotSession(b["id"])
+
+        for _, post_id in random_ids:
+            if(bot_session.Like(post_id)):
+                print("Curti um comentário")
+                sleep(TIME_BETWEEN_LIKES_ON_COMMENTS)
+            else:
+                print("Não curti um comentário")
+
+        bot_session.Close()
+
+    video_id = -1
+
+
+def GetRandomIds(exclude_id):
+    global video_id, comments_id
+    
+    comments_id_copy = comments_id.copy()
+    
+    try:
+        del comments_id_copy[exclude_id]
+    except:
+        pass
+
+    return sample(comments_id_copy.items(), MAX_LIKES_ON_COMMENTS if len(list(comments_id_copy.items())) > MAX_LIKES_ON_COMMENTS else len(list(comments_id_copy.items())))
+    
