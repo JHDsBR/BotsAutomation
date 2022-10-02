cc846e1674c267daf00eb5f54efa38da87e4fbe3
JHDsBR <Jesse2013.jsc@gmail.com>
2022-10-02 19:37:01 -0300

    Correção de bug

--- YoutubeAPI.py
+++ YoutubeAPI.py
@@ -7,6 +7,6 @@ from constants import MY_YT_API_KEY
 #! REVISADO
 
 def Search(search):
-    # r = rq.get(f"https://youtube.googleapis.com/youtube/v3/search?relevanceLanguage=pt&part=snippet&order=date&q={'%'.join(search.split())}&key={MY_YT_API_KEY}")
-    r = Request(rq, f"https://youtube.googleapis.com/youtube/v3/search?relevanceLanguage=pt&part=snippet&order=date&q={'%'.join(search.split())}&key={MY_YT_API_KEY}")
+    r = rq.get(f"https://youtube.googleapis.com/youtube/v3/search?relevanceLanguage=pt&part=snippet&order=date&q={'%'.join(search.split())}&key={MY_YT_API_KEY}")
+    # r = Request(rq, f"https://youtube.googleapis.com/youtube/v3/search?relevanceLanguage=pt&part=snippet&order=date&q={'%'.join(search.split())}&key={MY_YT_API_KEY}")
     return r
